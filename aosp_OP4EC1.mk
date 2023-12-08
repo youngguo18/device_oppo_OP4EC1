dd8ba0fa095d444bf22a9fdb12804c1df53cbb61
youngguo18 <guoamao@163.com>
2023-12-08 15:04:14 +0800

    Revert "OP4EC1: Dont build AudioFX"
    This reverts commit bfc0a26daf19c006628f6de97c537ac959bf87b2.

--- aosp_OP4EC1.mk
+++ aosp_OP4EC1.mk
@@ -26,7 +26,6 @@ TARGET_INCLUDE_PIXEL_CHARGER := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 EXTRA_UDFPS_ANIMATIONS := true
-TARGET_EXCLUDES_AUDIOFX := true
 
 # Inherit from device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
